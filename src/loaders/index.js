c324d7802bc71407a5994c9d80467b841f09a0b7
Vadym Babik <vadym.babik@gmail.com>
2021-07-13 09:34:41 +0300

    init

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,9 +1,9 @@
 import expressLoader from "./expressLoader";
-import mongoDBLoader from "./mongodb";
+// import mongoDBLoader from "./mongodb";
 
 export default async ({ config }) => {
-  await mongoDBLoader({ config });
-  console.log("MongoDB loaded and connected...");
+  // await mongoDBLoader({ config });
+  // console.log("MongoDB loaded and connected...");
   const app = expressLoader({ config });
   console.log("Express successfully configured and connected...");
   return await app;
